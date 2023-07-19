2130ad6f25aa6d8877253e15960ba466bf1efcc8
AnandRoy01 <anandroyyo@gmail.com>
2023-07-19 14:11:35 +0530

    FilterableProduct

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+
+const SearchBar = ({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange,
+}) => {
+  return (
+    <form>
+      <input
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)}
+      />
+      <label>
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => onInStockOnlyChange(e.target.checked)}
+        />{" "}
+        Only show products in stock
+      </label>
+    </form>
+  );
+};
+
+SearchBar.propTypes = {
+  filterText: PropTypes.string,
+  inStockOnly: PropTypes.bool,
+  onFilterTextChange: PropTypes.func,
+  onInStockOnlyChange: PropTypes.func,
+};
+
+export default SearchBar;
